fd80ab607376a95934a006c9478611e7c3bb0cdb
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2025-09-17 22:19:05 +0200

    Add SceneFader

--- Runtime/Components/SceneFader/JFadeEffect.cs
+++ Runtime/Components/SceneFader/JFadeEffect.cs
@@ -1,16 +1,104 @@
+using System;
 using UnityEngine;
 
+/// <summary>
+/// Abstract class for fade effects. Inherit from this class to implement specific fade effects.
+/// </summary>
 public abstract class JFadeEffect : MonoBehaviour
 {
-    private enum FadeState
+    protected enum FadeState
     {
         NONE,
         FADING_IN,
         FADING_OUT
     }
 
-    private FadeState currentFadeState = FadeState.NONE;
+    [SerializeField] protected float fadeInDuration = 1f;
+    [SerializeField] protected float fadeOutDuration = 1f;
+    protected float fadeTimer = 0f;
+    protected FadeState currentFadeState = FadeState.NONE;
 
-    public abstract void FadeIn(float duration);
-    public abstract void FadeOut(float duration);
+    private Action onFadeInComplete;
+    private Action onFadeOutComplete;
+
+    private void Update()
+    {
+        if (currentFadeState == FadeState.NONE) return;
+
+        fadeTimer += Time.deltaTime;
+        FadeAnimation();
+        switch (currentFadeState)
+        {
+            case FadeState.FADING_IN:
+                if (fadeTimer >= fadeInDuration)
+                {
+                    currentFadeState = FadeState.NONE;
+                    onFadeInComplete?.Invoke();
+                }
+                break;
+            case FadeState.FADING_OUT:
+                if (fadeTimer >= fadeOutDuration)
+                {
+                    currentFadeState = FadeState.NONE;
+                    onFadeOutComplete?.Invoke();
+                }
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Starts the fade-in effect.
+    /// </summary>
+    /// <param name="onFadeInComplete"></param>
+    public void FadeIn(Action onFadeInComplete = null)
+    {
+        if (currentFadeState == FadeState.FADING_IN) return;
+
+        currentFadeState = FadeState.FADING_IN;
+        fadeTimer = 0f;
+        this.onFadeInComplete = onFadeInComplete;
+        OnFadeIn();
+    }
+
+    /// <summary>
+    /// Starts the fade-out effect.
+    /// </summary>
+    /// <param name="onFadeOutComplete"></param>
+    public void FadeOut(Action onFadeOutComplete = null)
+    {
+        if (currentFadeState == FadeState.FADING_OUT) return;
+
+        currentFadeState = FadeState.FADING_OUT;
+        fadeTimer = 0f;
+        this.onFadeOutComplete = onFadeOutComplete;
+        OnFadeOut();
+    }
+
+    /// <summary>
+    /// Stops any ongoing fade effect.
+    /// </summary>
+    /// <param name="onFadeStop"></param>
+    public void StopFade(Action onFadeStop = null)
+    {
+        currentFadeState = FadeState.NONE;
+        fadeTimer = 0f;
+        onFadeInComplete = null;
+        onFadeOutComplete = null;
+        onFadeStop?.Invoke();
+    }
+
+    /// <summary>
+    /// Method to be called to perform the fade animation.
+    /// </summary>
+    abstract protected void FadeAnimation();
+
+    /// <summary>
+    /// Called when the fade-in effect starts.
+    /// </summary>
+    abstract protected void OnFadeIn();
+
+    /// <summary>
+    /// Called when the fade-out effect starts.
+    /// </summary>
+    abstract protected void OnFadeOut();
 }
--- Runtime/Components/SceneFader/JSceneFader.cs
+++ Runtime/Components/SceneFader/JSceneFader.cs
@@ -1,6 +1,81 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Singleton component to manage scene fading using a specified fade effect.
+/// </summary>
 public class JSceneFader : MonoBehaviour
 {
+    [SerializeField] private bool isPersistent = true;
 
+    [Space]
+    [SerializeField] private JFadeEffect fadeEffect;
+
+    public static JSceneFader Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            if (isPersistent)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Sets the fade effect to be used for scene transitions.
+    /// </summary>
+    /// <param name="newFadeEffect"></param>
+    public void SetFadeEffect(JFadeEffect newFadeEffect)
+    {
+        fadeEffect = newFadeEffect;
+    }
+
+    /// <summary>
+    /// Starts the fade-in effect.
+    /// </summary>
+    /// <param name="onFadeInComplete"></param>
+    public void FadeIn(Action onFadeInComplete = null)
+    {
+        fadeEffect.FadeIn(onFadeInComplete);
+    }
+
+    /// <summary>
+    /// Starts the fade-out effect.
+    /// </summary>
+    /// <param name="onFadeOutComplete"></param>
+    public void FadeOut(Action onFadeOutComplete = null)
+    {
+        fadeEffect.FadeOut(onFadeOutComplete);
+    }
+
+    /// <summary>
+    /// Fades to a new scene with fade-in and fade-out effects.
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void FadeToScene(string sceneName)
+    {
+        fadeEffect.FadeIn(() =>
+        {
+            SceneManager.LoadScene(sceneName);
+            fadeEffect.FadeOut();
+        });
+    }
+
+    /// <summary>
+    /// Stops any ongoing fade effect.
+    /// </summary>
+    /// <param name="onFadeStop"></param>
+    public void StopFade(Action onFadeStop = null)
+    {
+        fadeEffect.StopFade(onFadeStop);
+    }
 }
